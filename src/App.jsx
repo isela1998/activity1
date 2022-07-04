b0dc1ac4caf7810d6f6f32e2f248dc48598e0eea
Isela Infante <infantefernandezisela@gmail.com>
2022-07-04 13:02:35 -0400

    div center

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,21 @@ function App() {
 
   const firstQuote = Math.floor(Math.random() * quotes.length)
   const [ramdomQuote, setRamdomQuote] = useState(firstQuote)
-  
+
   const nextQuote = () => {
     const newQuote = Math.floor(Math.random() * quotes.length)
     setRamdomQuote(newQuote)
   }
-  
+
   const ramdomColor = Math.floor(Math.random() * colors.length)
   document.body.style = `background: ${colors[ramdomColor]}`;
-  
+
   return (
-    <div className='card'>
-      <QuoteBox textColor={colors[ramdomColor]} quote={quotes[ramdomQuote].quote} author={quotes[ramdomQuote].author}/>
-      <NextButton textColor={colors[ramdomColor]} changeQuote={nextQuote} />
+    <div className='App'>
+      <div className='card'>
+        <QuoteBox textColor={colors[ramdomColor]} quote={quotes[ramdomQuote].quote} author={quotes[ramdomQuote].author} />
+        <NextButton textColor={colors[ramdomColor]} changeQuote={nextQuote} />
+      </div>
     </div>
   )
 }
